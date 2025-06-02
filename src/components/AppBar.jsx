919fc8c4bea9d8aa7c7cf9f2d687331360dddba0
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2025-06-02 22:44:05 +0300

    Exercise 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,33 +1,35 @@
 import { View, StyleSheet, Text, Pressable } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
+    ...theme.horizontalContainer,
     paddingTop: Constants.statusBarHeight,
     paddingBottom: 10,
     paddingLeft: 10,
     backgroundColor: theme.colors.appBar,
   },
   tab: {
-    paddingTop: 20,
     color: "#ffffff",
     fontSize: 20,
   },
 });
 
-const AppBarTab = ({ tabTitle }) => {
+const AppBarTab = ({ tabTitle, tabTarget }) => {
   return (
-    <Pressable onPress={() => console.log("Repositories pressed")}>
+    <Link to={tabTarget}>
       <Text style={styles.tab}>{tabTitle}</Text>
-    </Pressable>
+    </Link>
   );
 };
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab tabTitle="Repositories" />
+      <AppBarTab tabTitle="Repositories" tabTarget="/" />
+      <AppBarTab tabTitle="Sign In" tabTarget="/signin" />
     </View>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,27 @@
-import { StyleSheet, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import { Route, Routes, Navigate } from "react-router-native";
+import SignIn from "./SignIn";
+import theme from "../theme";
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
-    marginTop: 20,
   },
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor={theme.colors.appBar} />
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
