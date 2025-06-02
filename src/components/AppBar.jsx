0f09e598d2e0f524556ad466c440062783842e14
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2025-06-02 19:25:45 +0300

    Exercise 10.4

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet, Text, Pressable } from "react-native";
+import Constants from "expo-constants";
+import theme from "./theme";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    backgroundColor: theme.appBar.backgroundColor,
+  },
+  tab: {
+    paddingTop: 20,
+    color: "#ffffff",
+    fontSize: 20,
+  },
+});
+
+const AppBarTab = ({ tabTitle }) => {
+  return (
+    <Pressable onPress={() => console.log("Repositories pressed")}>
+      <Text style={styles.tab}>{tabTitle}</Text>
+    </Pressable>
+  );
+};
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <AppBarTab tabTitle="Repositories" />
+    </View>
+  );
+};
+
+export default AppBar;
